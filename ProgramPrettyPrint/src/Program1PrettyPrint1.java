3095c174f70303582714cc137b83b86394d75ce3
Frank <Frank@164.107.9.19>
2023-10-23 14:48:49 -0400

    FC

---
+++ ProgramPrettyPrint/src/Program1PrettyPrint1.java
@@ -0,0 +1,149 @@
+import components.map.Map;
+import components.map.Map1L;
+import components.program.Program;
+import components.program.Program1;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+import components.statement.Statement;
+import components.statement.Statement1;
+
+/**
+ * Layered implementation of secondary method {@code prettyPrint} for
+ * {@code Program}.
+ */
+public final class Program1PrettyPrint1 extends Program1 {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    /**
+     * Constructs into the given {@code Program} the program read from the given
+     * input file.
+     *
+     * @param fileName
+     *            the name of the file containing the program
+     * @param p
+     *            the constructed program
+     * @replaces p
+     * @requires [fileName is the name of a file containing a valid BL program]
+     * @ensures p = [program from file fileName]
+     */
+    private static void loadProgram(String fileName, Program p) {
+        SimpleReader in = new SimpleReader1L(fileName);
+        p.parse(in);
+        in.close();
+    }
+
+    /**
+     * Prints the given number of spaces to the given output stream.
+     *
+     * @param out
+     *            the output stream
+     * @param numSpaces
+     *            the number of spaces to print
+     * @updates out.content
+     * @requires out.is_open and spaces >= 0
+     * @ensures out.content = #out.content * [numSpaces spaces]
+     */
+    private static void printSpaces(SimpleWriter out, int numSpaces) {
+        for (int i = 0; i < numSpaces; i++) {
+            out.print(' ');
+        }
+    }
+
+    /*
+     * Constructors -----------------------------------------------------------
+     */
+
+    /**
+     * No-argument constructor.
+     */
+    public Program1PrettyPrint1() {
+        super();
+    }
+
+    /*
+     * Secondary methods ------------------------------------------------------
+     */
+
+    @Override
+    public void prettyPrint(SimpleWriter out) {
+        assert out != null : "Violation of: out is not null";
+        assert out.isOpen() : "Violation of: out.is_open";
+
+        // TODO - fill in body
+        Statement s = new Statement1();
+        Map<String, Statement> m = new Map1L<String, Statement>();
+        this.swapBody(s);
+        this.swapContext(m);
+        out.println("PROGRAM " + this.name() + " IS");
+        out.println();
+        for (Map.Pair<String, Statement> p : m) {
+            out.println("    INSTRUCTION " + p.key() + " IS");
+            p.value().prettyPrint(out, INDENT_SIZE + 4);
+            out.println("    END " + p.key());
+            out.println();
+
+        }
+        out.println("BEGIN");
+        s.prettyPrint(out, INDENT_SIZE);
+        out.println("END " + this.name());
+        this.swapBody(s);
+        this.swapContext(m);
+
+    }
+
+    /*
+     * Main test method -------------------------------------------------------
+     */
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Get input file name
+         */
+        out.print("Enter valid BL program file name: ");
+        String fileName = in.nextLine();
+        //String fileName = "data/SampleProgram.bl";
+        /*
+         * Generate expected output in file "data/expected-output.txt"
+         */
+        out.println("*** Generating expected output ***");
+        Program p1 = new Program1();
+        loadProgram(fileName, p1);
+        SimpleWriter ppOut = new SimpleWriter1L("data/expected-output.txt");
+        p1.prettyPrint(ppOut);
+        ppOut.close();
+        /*
+         * Generate actual output in file "data/actual-output.txt"
+         */
+        out.println("*** Generating actual output ***");
+        Program p2 = new Program1PrettyPrint1();
+        loadProgram(fileName, p2);
+        ppOut = new SimpleWriter1L("data/actual-output.txt");
+        p2.prettyPrint(ppOut);
+        ppOut.close();
+        /*
+         * Check that prettyPrint restored the value of the program
+         */
+        if (p2.equals(p1)) {
+            out.println("Program value restored correctly.");
+        } else {
+            out.println("Error: program value was not restored.");
+        }
+
+        in.close();
+        out.close();
+    }
+
+}
